1440870157e13cf0a063c911e5d7d37324feb707
abner506 <abnercorona9@hotmail.com>
2021-04-20 01:03:43 -0500

    Subiento graficas01

---
+++ app/Http/Requests/validaRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+use Illuminate\Foundation\Http\FormRequest;
+class validaRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'Matricula' => 'required|max:10',
+            'Nombre' => 'required|regex:/^[\pL\s\-]+$/u',
+            'Direccion' => 'required|regex:/^([0-9a-zA-ZñÑáéíóúÁÉÍÓÚ_-])+((\s*)+([0-9a-zA-ZñÑáéíóúÁÉÍÓÚ_-]*)*)+$/',
+        ];
+    }
+}
